9680e3d8cca823a6479ba412b7309833722639d4
naithy <naithyDev@gmail.com>
2023-06-01 19:30:30 +0400

    init

--- index.js
+++ index.js
@@ -11,9 +11,36 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+const getInvoice = (id) => {
+    return {
+        chat_id: id, // Уникальный идентификатор целевого чата или имя пользователя целевого канала
+        provider_token: '401643678:TEST:ad357d6e-c472-49de-b6f3-e45f39da0951', // токен выданный через бот @SberbankPaymentBot
+        start_parameter: 'get_access', //Уникальный параметр глубинных ссылок. Если оставить поле пустым, переадресованные копии отправленного сообщения будут иметь кнопку «Оплатить», позволяющую нескольким пользователям производить оплату непосредственно из пересылаемого сообщения, используя один и тот же счет. Если не пусто, перенаправленные копии отправленного сообщения будут иметь кнопку URL с глубокой ссылкой на бота (вместо кнопки оплаты) со значением, используемым в качестве начального параметра.
+        title: 'InvoiceTitle', // Название продукта, 1-32 символа
+        description: 'InvoiceDescription', // Описание продукта, 1-255 знаков
+        currency: 'RUB', // Трехбуквенный код валюты ISO 4217
+        prices: [{label: 'Invoice Title', amount: 100 * 100}], // Разбивка цен, сериализованный список компонентов в формате JSON 100 копеек * 100 = 100 рублей
+        photo_url: 'https://s3.eu-central-1.wasabisys.com/ghashtag/JavaScriptBot/Unlock.png', // URL фотографии товара для счета-фактуры. Это может быть фотография товара или рекламное изображение услуги. Людям больше нравится, когда они видят, за что платят.
+        photo_width: 500, // Ширина фото
+        photo_height: 281, // Длина фото
+        payload: { // Полезные данные счета-фактуры, определенные ботом, 1–128 байт. Это не будет отображаться пользователю, используйте его для своих внутренних процессов.
+            unique_id: `${id}_${Number(new Date())}`,
+            provider_token: '401643678:TEST:ad357d6e-c472-49de-b6f3-e45f39da0951'
+        }
+    }
+}
+
+
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
+
+
+    if (text === '/start') {
+        await bot.sendInvoice(getInvoice(chatId))
+    }
+
 
     if(text === '/start') {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
@@ -56,7 +83,7 @@ app.post('/web-data', async (req, res) => {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
-            input_message_content: {message_text: 'Вы приобрели товар на сумму: ' + totalPrice}
+            input_message_content: {message_text: `Вы приобрели товар на сумму: ${totalPrice}`}
         })
         return res.status(200).json({});
     } catch (e) {
