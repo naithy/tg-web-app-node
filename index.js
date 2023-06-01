52ac86c7b6ea4daff08da134f9bc79f6626383a7
naithy <naithyDev@gmail.com>
2023-06-01 23:46:54 +0400

    init

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express')
 const cors = require('cors')
 
+
 const token = '6206628203:AAGKvS-tRT3BKXP2YVxUOb0tH1tfFlvYxC8';
-const webAppUrl ='https://master--magical-moxie-a1c933.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
@@ -12,92 +12,51 @@ app.use(express.json());
 app.use(cors());
 
 
-const getInvoice = (id) => {
-    return {
-        chat_id: id, // Уникальный идентификатор целевого чата или имя пользователя целевого канала
-        provider_token: '401643678:TEST:ad357d6e-c472-49de-b6f3-e45f39da0951', // токен выданный через бот @SberbankPaymentBot
-        start_parameter: 'get_access', //Уникальный параметр глубинных ссылок. Если оставить поле пустым, переадресованные копии отправленного сообщения будут иметь кнопку «Оплатить», позволяющую нескольким пользователям производить оплату непосредственно из пересылаемого сообщения, используя один и тот же счет. Если не пусто, перенаправленные копии отправленного сообщения будут иметь кнопку URL с глубокой ссылкой на бота (вместо кнопки оплаты) со значением, используемым в качестве начального параметра.
-        title: 'InvoiceTitle', // Название продукта, 1-32 символа
-        description: 'InvoiceDescription', // Описание продукта, 1-255 знаков
-        currency: 'RUB', // Трехбуквенный код валюты ISO 4217
-        prices: [{label: 'Invoice Title', amount: 100 * 100}], // Разбивка цен, сериализованный список компонентов в формате JSON 100 копеек * 100 = 100 рублей
-        photo_url: 'https://s3.eu-central-1.wasabisys.com/ghashtag/JavaScriptBot/Unlock.png', // URL фотографии товара для счета-фактуры. Это может быть фотография товара или рекламное изображение услуги. Людям больше нравится, когда они видят, за что платят.
-        photo_width: 500, // Ширина фото
-        photo_height: 281, // Длина фото
-        payload: { // Полезные данные счета-фактуры, определенные ботом, 1–128 байт. Это не будет отображаться пользователю, используйте его для своих внутренних процессов.
-            unique_id: `${id}_${Number(new Date())}`,
-            provider_token: '401643678:TEST:ad357d6e-c472-49de-b6f3-e45f39da0951'
-        }
-    }
-}
 
-
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
-
-
-    if (text === '/start') {
-        await bot.sendInvoice(getInvoice(chatId))
-    }
-
-
-    if(text === '/start') {
-        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
-            reply_markup: {
-                keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
-                ]
-            }
-        })
-
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-                ]
-            }
-        })
-    }
-
-    if(msg?.web_app_data?.data) {
-        try {
-            const data = JSON.parse(msg?.web_app_data?.data)
-            console.log(data)
-            await bot.sendMessage(chatId,'Данные получены.')
-
-            setTimeout(async () => {
-                await bot.sendMessage(chatId, data?.name)
-            }, 3000)
-
-        } catch (e) {
-            console.log(e)
-        }
-    }
-});
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products, totalPrice} = req.body;
+    const {totalPrice, cart} = req.body;
     try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {message_text: `Вы приобрели товар на сумму: ${totalPrice}`}
-        })
-        return res.status(200).json({});
-    } catch (e) {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Не удалось приобрести товар',
-            input_message_content: {message_text: 'Не удалось приобрести товар'}
-        })
-        return res.status(500).json({});
-    }
+        const newCart = Object.entries(cart).reduce((acc, [key, item]) => {
+            const { flavors, ...rest } = item;
+            Object.keys(flavors).forEach(flavor => {
+                acc.push({
+                    title: `${rest.title} ${flavor}`,
+                    price: rest.price * 100
+                });
+            });
 
+            return acc;
+        }, []);
+        products = newCart
+    } catch (e) {
+        console.log('error')
+    }
 })
 
-const PORT = 8000;
+const title = 'Заказ #1313'
+let products;
 
+
+bot.onText(/\/pay/, msg => {
+    const chatId = msg.chat.id
+    bot.sendInvoice(
+        chatId,
+        title,
+        'dssd',
+        'dsds',
+        '1744374395:TEST:b86b8d2a26f4473364a2',
+        'RUB',
+        products
+    )
+})
+
+bot.on('pre_checkout_query', (query) => {
+    bot.answerPreCheckoutQuery(query.id, true)
+})
+bot.on('successful_payment', function (msg) {
+    bot.sendMessage(chatId, `Payment for ${msg.successful_payment.total_amount} ${msg.successful_payment.currency} received!`);
+});
+
+const PORT = 8000;
 app.listen(PORT, () => console.log('Server started on port:' + PORT));
