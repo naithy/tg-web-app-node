80e1b03ea00fbd71fa6834ccda1b85b2293453fe
naithy <naithyDev@gmail.com>
2023-06-03 14:56:21 +0400

    init

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express')
 const cors = require('cors')
-const { createProxyMiddleware } = require('http-proxy-middleware');
+
 
 const token = '6206628203:AAGKvS-tRT3BKXP2YVxUOb0tH1tfFlvYxC8';
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const target = 'https://77.105.172.20:8000';
-
-
-const proxy = createProxyMiddleware({
-    target,
+const httpProxy = createProxyMiddleware({
+    target: 'http://77.105.172.20:8000', // адрес вашего HTTP-приложения
     changeOrigin: true,
-    secure: false
 });
 
-app.use('/api', proxy);
+
 app.use(express.json());
 app.use(cors());
-
+app.use('/web-data', httpProxy)
 
 app.post('/web-data', async (req, res) => {
     const {queryID, cart} = req.body;
