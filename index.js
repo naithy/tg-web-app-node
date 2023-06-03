728f69dbb4e1a20a525c884dff0fcef10b70613b
naithy <naithyDev@gmail.com>
2023-06-03 14:03:22 +0400

    init

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express')
 const cors = require('cors')
-const https = require('https');
-const fs = require('fs');
-const options = {
-    cert: fs.readFileSync('./sslcert/fullchain.pem'),
-    key: fs.readFileSync('./sslcert/privkey.pem')
-};
+
 
 const token = '6206628203:AAGKvS-tRT3BKXP2YVxUOb0tH1tfFlvYxC8';
 
@@ -33,4 +28,3 @@ const title = 'Заказ #1313'
 
 const PORT = 8000;
 app.listen(PORT, () => console.log('Server started on port:' + PORT));
-https.createServer(options, app).listen(8443);
