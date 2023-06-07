485510aca2fc5c66c48a931d8f272d1c20e8d18b
naithy <naithyDev@gmail.com>
2023-06-07 17:35:00 +0400

    init

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ const express = require('express');
 const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
+const mongoose = require('mongoose');
+const Customer = require('./models/Customer')
 
 const options = {
-    cert: fs.readFileSync('fullchain.pem'),
-    key: fs.readFileSync('privkey.pem')
+    // cert: fs.readFileSync('fullchain.pem'),
+    // key: fs.readFileSync('privkey.pem')
 };
 
 const token = '6206628203:AAGKvS-tRT3BKXP2YVxUOb0tH1tfFlvYxC8';
-const webAppUrl = 'https://sakurashop.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
+const server = https.createServer(options, app)
+
+app.use(express.json());
+app.use(cors());
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://94.198.217.174:27017/test', {useNewUrlParser: true})
+        app.listen(443, () => {
+            console.log('Server running 443 port')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -23,27 +39,23 @@ bot.on("callback_query", (query) => {
     const queryChatId = query.message.chat.id;
     const messageId = query.message.message_id;
     if (query.data === "delete") {
+
         bot.deleteMessage(queryChatId, messageId);
     }
 });
 
-app.use(express.json());
-app.use(cors());
-
-const server = https.createServer(options, app)
-
 app.post('/web-data', async (req, res) => {
-    const {queryId, user, totalPrice, cart} = req.body;
+    const {queryId, user, totalPrice, cart, chat} = req.body;
     try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}`
-            }
-        })
-        await bot.sendMessage(5212881326, `Клиент *${user.first_name} ${user?.last_name}* ${user.username ? '@' + user.username : ''}\nCписок товаров: ${
+        // await bot.answerWebAppQuery(queryId, {
+        //     type: 'article',
+        //     id: queryId,
+        //     title: 'Успешная покупка',
+        //     input_message_content: {
+        //         message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}`
+        //     }
+        // })
+        await bot.sendMessage(5212881326, `\nCписок товаров: ${
             Object.values(cart)
                 .map((value, ) => {
                     const flavors = Object.entries(value.flavors)
@@ -56,11 +68,31 @@ app.post('/web-data', async (req, res) => {
             {parse_mode: 'markdown',
                 reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Удалить',  callback_data: "delete"}]
+                    [{text: 'Подтвердить', callback_data: "delete"}, {text: 'Отменить',  callback_data: "delete"}]
                 ]
                 }
 
             })
+
+        for (const key in cart) {
+            if (cart.hasOwnProperty(key)) {
+                try {
+                    const customer = new Customer({
+                        user: user,
+                        chat_id: chat,
+                        title: cart[key].title,
+                        price: cart[key].price,
+                        flavors: cart[key].flavors
+                    });
+                    customer.save().then(() => console.log('User saved'))
+                }   catch (e) {
+                    console.log(e)
+                }
+
+
+            }
+        }
+
         return res.status(200).json({});
     } catch (e) {
         console.log('error')
@@ -68,6 +100,5 @@ app.post('/web-data', async (req, res) => {
     }
 })
 
-server.listen(443, () => {
-    console.log('Server running 443 port')
-})
+
+start()
