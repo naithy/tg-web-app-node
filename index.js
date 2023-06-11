665eed11c910c9e23f1650dd1d8b95f2d46b6a09
naithy <naithyDev@gmail.com>
2023-06-11 16:19:40 +0400

    init

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ const start = async () => {
 }
 
 io.on('connection', async (socket) => {
-    console.log('connected');
     try {
         const customers = await Customer.find();
         socket.emit('items', customers);
@@ -65,17 +64,14 @@ io.on('connection', async (socket) => {
                 createdAt: change.fullDocument.createdAt,
             };
             socket.emit('changeData', customer);
-            console.log('sended');
         } else if (change.operationType === 'update') {
             const updatedCustomer = {
                 _id: change.documentKey._id,
                 ...change.updateDescription.updatedFields,
             };
             socket.emit('updateData', updatedCustomer);
-            console.log('updated');
         } else if (change.operationType === 'delete') {
             socket.emit('deleteData', change.documentKey._id);
-            console.log('deleted');
         }
     });
 });
