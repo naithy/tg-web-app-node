b5eaa6a762a73dceffde9ba9dd483ce76fb9d62d
naithy <naithyDev@gmail.com>
2023-06-11 13:33:42 +0400

    init

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ changeStream.on('change', (change) => {
             createdAt: change.fullDocument.createdAt,
         }]
         console.log(customer);
-        io.emit('items', change);
+        io.emit('items', customer);
     }
 });
 
