e92015908734b57e3608650cb30060d719248a83
naithy <naithyDev@gmail.com>
2023-06-11 15:46:56 +0400

    init

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post('/web-data', async (req, res) => {
 
         return res.status(200).json({});
     } catch (e) {
-        console.log('error')
+        console.log(e)
         return res.status(500).json({})
     }
 });
