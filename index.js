6ff99247e4d6abf091daa7674a003da3b832bf55
naithy <naithyDev@gmail.com>
2023-06-11 13:20:25 +0400

    init

--- index.js
+++ index.js
@@ -45,19 +45,20 @@ const start = async () => {
 
 const changeStream = Customer.watch()
 changeStream.on('change', (change) => {
-    // const customer = {
-    //     _id: change.fullDocument._id,
-    //     first_name: change.fullDocument.first_name,
-    //     username: change.fullDocument.username,
-    //     totalPrice: change.fullDocument.totalPrice,
-    //     cart: change.fullDocument.cart,
-    //     birthday: change.fullDocument.birthday,
-    //     number: change.fullDocument.number,
-    //     createdAt: change.fullDocument.createdAt,
-    // }
-
-    console.log(change);
-    io.emit('changeData', change);
+    if (change.operationType === 'insert') {
+        const customer = {
+            _id: change.fullDocument._id,
+            first_name: change.fullDocument.first_name,
+            username: change.fullDocument.username,
+            totalPrice: change.fullDocument.totalPrice,
+            cart: change.fullDocument.cart,
+            birthday: change.fullDocument.birthday,
+            number: change.fullDocument.number,
+            createdAt: change.fullDocument.createdAt,
+        }
+        console.log(customer);
+        io.emit('changeData', change);
+    }
 });
 
 io.on('connection', function () {
