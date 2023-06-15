e2011f7a43054c383febc831b312b52b73863271
naithy <naithyDev@gmail.com>
2023-06-15 19:03:08 +0400

    init

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ app.post('/product', async (req, res) => {
 
 app.delete('/product', async (req, res) => {
     const {id} = req.body
-    console.log(id)
+    console.log(req.body)
     try {
         Product.deleteOne({_id: id})
     } catch (e) {
