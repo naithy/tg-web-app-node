326ac3c8e3c448c29a5904ae4ee4f15cbdba8f75
naithy <naithyDev@gmail.com>
2023-06-16 22:45:45 +0400

    init

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ const start = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
-        server.listen(443, () => {
-            console.log('Server running 443 port')
+        server.listen(8000, () => {
+            console.log('Server running 8000 port')
         })
     } catch (e) {
         console.log(e)
