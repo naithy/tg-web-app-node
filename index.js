b5f00bb8bfe011c4d4ccac8cd6ceb4847a8dc91e
naithy <naithyDev@gmail.com>
2023-06-17 19:17:29 +0400

    init

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = https.createServer(options, app)
 
 const io = socketIo(server, {
     cors: {
-        origin: 'https://sakurashopadmin260119.netlify.app'
+        origin: 'http://localhost:3000'
     }
 });
 
