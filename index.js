dd3b956581a50cff2ab689f93df4fa9762205538
naithy <naithyDev@gmail.com>
2023-06-18 00:18:48 +0400

    init

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = http.createServer(app);
 
 const start = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/test', {
+        await mongoose.connect('mongodb://127.0.0.1:27017/test', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
